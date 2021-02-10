111eb32d1329b5509044584912fe4eade98e696f
obddd <obaidulhaq20@gmail.com>
2021-02-10 23:45:42 +0500

    fetched data using page query

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import HeaderHooks from '../examples/Header-Hooks';
-import HeaderStatic from '../examples/Header-Static';
-const Examples = () => {
-    return (
-        <div>
-            <HeaderHooks/>
-            <HeaderStatic/>
-        </div>
-    );
-};
+import React from "react"
+import HeaderHooks from "../examples/Header-Hooks"
+import HeaderStatic from "../examples/Header-Static"
+import { graphql } from "gatsby"
+const Examples = props => {
+  const { description } = props.data.site.siteMetadata
+  return (
+    <div>
+      <HeaderHooks />
+      <HeaderStatic />
+      <div>
+        I am using page query
+        <h3>{description}</h3>
+      </div>
+    </div>
+  )
+}
 
-export default Examples;
+export const getData = graphql`
+  query {
+    site {
+      siteMetadata {
+        description
+      }
+    }
+  }
+`
+
+export default Examples
