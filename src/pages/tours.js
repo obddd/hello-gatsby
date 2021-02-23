0daf697347f5664ca2e80421a39b815bf107863b
obddd <obaidulhaq20@gmail.com>
2021-02-23 22:55:40 +0500

    setup tour query

--- src/pages/tours.js
+++ src/pages/tours.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import Tours from '../components/Tours/Tours';
-import Layout from '../components/layout'
+import React from "react"
+import Tours from "../components/Tours/Tours"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-const tours = () => {
-    return (
-        <Layout>
-            This is tour page
-            <Tours/>
-        </Layout>
-    );
-};
+const tours = ({ data }) => {
+  return (
+    <Layout>
+      This is tour page
+      <Tours tours={data.tours.edges} />
+    </Layout>
+  )
+}
 
-export default tours;
+export const getTours = graphql`
+  query{
+  tours: allContentfulTour{
+    edges{
+      node{
+        id: contentful_id
+        name 
+        price
+        country
+        images{
+          fluid{
+            src
+          }
+        }
+      }
+    }
+  }
+}
+`
+
+export default tours
