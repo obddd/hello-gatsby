ad5e3cab9384ce398c957796b44278d0a5af4d14
obddd <obaidulhaq20@gmail.com>
2021-03-06 15:17:59 +0500

    using lib in functions

--- functions/hello/hello.js
+++ functions/hello/hello.js
@@ -1,10 +1,13 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const random = require('random-name');
+
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    // const subject = event.queryStringParameters.name || 'World'
+    let name = random.first()
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Updated Hello ${subject}` }),
+      body: JSON.stringify({ message: `Hello ${name} from netlify` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
